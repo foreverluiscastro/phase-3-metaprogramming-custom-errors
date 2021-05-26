7ddbe1879e081e1b40c804db8a8408b05369eda6
luis <foreverluiscastro@gmail.com>
2021-05-25 21:27:23 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,9 +7,25 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    # We need to tell our program to raise our brand new `PartnerError` when the argument passed into the `#get_married` method is *not* an instance of the `Person` class. We can do that with the `raise` keyword.
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
 
+  # Add the following two lines:
+  class PartnerError < StandardError
+    # Of course we want our program to continue running after we raise the error. It would also be nice to output a custom error message when the error is raised.
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
